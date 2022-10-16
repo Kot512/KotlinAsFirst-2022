278e3e61c242b2eb50b14b3d05f545bb50d23889
kot20 <kot20247@gmail.com>
2022-10-16 11:34:30 +0300

    5 урок подправлен

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -395,19 +395,19 @@ fun bagPacking(treasures: Map<String, Pair<Int, Int>>, capacity: Int): Set<Strin
 
     var currentCapacity2 = capacity
     val priorityList2 =
-        treasures.values.sortedWith(compareBy({ it.second }, { it.first }))
+        treasures.values.sortedWith(compareBy({ -it.second / it.first }, { it.first }))
     val backpack2 = treasures.toMutableMap()
     var currentPrice2 = 0
 
     //заполнение рюкзака, начиная с самого ценного сокровища
-    /*for (i in priorityList1) {
+    for (i in priorityList1) {
         if (currentCapacity1 - i.first < 0)
             backpack1 -= (backpack1.filterValues { it == i }.keys.last())
         else {
             currentCapacity1 -= i.first
             currentPrice1 += i.second
         }
-    }*/
+    }
 
     //заполнение рюкзака, начиная с самого дешевого сокровища
     for (i in priorityList2) {
