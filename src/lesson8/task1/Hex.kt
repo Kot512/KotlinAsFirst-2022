2e11a3717f36696a6554f07bcf1b074ea65b8133
kot20 <kot20247@gmail.com>
2022-12-21 09:51:12 +0300

    8 урок

--- src/lesson8/task1/Hex.kt
+++ src/lesson8/task1/Hex.kt
@@ -305,7 +305,7 @@ fun minContainingHexagon(vararg points: HexPoint): Hexagon {
         // погрешности в каждую сторону
 
         val centersWithInaccuracies = mutableListOf(curCenter)
-        if (inaccuracy < 200000) {
+        if (inaccuracy < 500000) {
             for (direction in Direction.values()) {
                 if (direction != Direction.INCORRECT)
                     centersWithInaccuracies += curCenter.move(direction, inaccuracy)
